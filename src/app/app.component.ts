a86720369c971f350bd713fb3077b9cb833a7098
Oleksandr <sasha.zikun@gmail.com>
2020-03-02 20:52:28 +0200

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'course';
+  title = 'qwwq';
+  counter = 1;
+  stateDropDown = false;
+  products = [
+    {
+      id: 1,
+      name: 'product 1',
+      price: '12 grn'
+    },
+    {
+      id: 2,
+      name: 'product 2',
+      price: '36 grn'
+    },
+    {
+      id: 3,
+      name: 'product 3',
+      price: '8 grn'
+    }
+  ];
+
+  newProduct = ''
+
+  addProduct() {
+    console.log(this.newProduct);
+    this.products.push({
+      id: 4,
+      name: this.newProduct,
+      price: '8 grn'
+    });
+  }
+
+  changeTitle() {
+    this.title = 'Lesson';
+    // this.title = !this.title;
+  }
+
+  incrementCounter() {
+    this.counter++;
+  }
+
+  toggleDropDown() {
+    this.stateDropDown = !this.stateDropDown;
+  }
 }
