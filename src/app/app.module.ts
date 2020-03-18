9c847b7d8d7608937bee2a86227e9a7ee7362112
OleksandrZaplatynskiy <sasha.zikun@gmail.com>
2020-03-18 21:54:48 +0200

    Lesson7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ProductSingleComponent } from './product-single/product-single.component';
+import { ProductLiComponent } from './product-li/product-li.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductItemComponent,
-    ProductSingleComponent
+    ProductSingleComponent,
+    ProductLiComponent,
+    PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-li/product-li.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../date.service';
+import { Product } from '../product.models';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-li',
+  templateUrl: './product-li.component.html',
+  styleUrls: ['./product-li.component.scss']
+})
+export class ProductLiComponent implements OnInit {
+
+  products: Product[];
+  formAddProduct: FormGroup;
+
+  constructor(
+    private dataService: DataService,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.products = this.dataService.getProducts();
+    // console.log(this.products); 
+    this.formAddProduct = this.fb.group({
+      name: [''],
+      price: ['']
+    })  
+  }
+
+  onAddProduct() {
+    this.dataService.addProduct(this.formAddProduct.value);
+  }
+
+  deleteProduct(id) {
+    this.dataService.deleteProduct(id);
+  }
+  
+}
